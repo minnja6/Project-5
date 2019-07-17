b1e3ce1055b97f53050ef605e315985d4acd06ee
minnja6 <jasmine.minniweather@verizon.com>
2019-07-17 13:59:56 -0500

    update to scrpt.js
    Update to script.js and got 12 cards to show with the info

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
+let employeeList = [];
 $.ajax({
     url: 'https://randomuser.me/api/?results=12',
     dataType: 'json',
     success: function(data) {
         console.log(data);
+        employeeList = data.results;
+        createAllCards(employeeList);
     }
 });
+function createAllCards(list){
+    for(let i = 0; i < list.length; i++){
+        createEmployeeCard(employeeList[i]);
+    }
+}
+function createEmployeeCard(employee){
+    let card = 
+        `<div class="card">
+            <div class="card-img-container">
+                <img class="card-img" src="${employee.picture.thumbnail}" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+                <p class="card-text">${employee.email}</p>
+                <p class="card-text cap">${employee.location.city},${employee.location.state}</p>
+            </div>
+        </div>`;
+    $('#gallery').append(card);
+}
