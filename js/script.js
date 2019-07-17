8b53d068f21dd7c9a5a2e2dc6cd109c5a3667368
minnja6 <jasmine.minniweather@verizon.com>
2019-07-17 15:36:28 -0500

    additions
    additions to script.js, modal

--- js/script.js
+++ js/script.js
@@ -27,3 +27,27 @@ function createEmployeeCard(employee){
         </div>`;
     $('#gallery').append(card);
 }
+function createmodal(list){
+    for(let i = 0; i < list.length; i++){
+        modal(employeeList[i]);
+    }
+}
+$('.modal__overlay').style = 'display: inline-block';
+    function modal(employee){
+    let modalContainer =
+    `<div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${employee.picture.thumbnail}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                    <p class="modal-text">${employee.email}</p>
+                    <p class="modal-text cap">${employee.location.city}</p>
+                    <hr>
+                    <p class="modal-text">${employee.cell}</p>
+                    <p class="modal-text">${employee.location.street}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+                    <p class="modal-text">${employee.dob.date}</p>
+                </div>
+            </div>;`
+            $('body').append(modalContainer);
+}
