66fe90675c99a6489ad80d6822ead23873e03b02
minnja6 <jasmine.minniweather@verizon.com>
2019-07-18 14:44:59 -0500

    modal is showing
    modal is showing, need to get the exit x working

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $.ajax({
 function createEmployeeCard(employeeList){
     for(let i = 0; i < employeeList.length; i++){
         let card = 
-        `<div class="card" index "${i}">
+        `<div class="card" index="${i}">
             <div class="card-img-container">
                 <img class="card-img" src="${employeeList[i].picture.thumbnail}" alt="profile picture">
             </div>
@@ -23,32 +23,35 @@ function createEmployeeCard(employeeList){
         </div>`;
     $('#gallery').append(card);
 }
-    function checkForName(){
-        if (element.classList.contains(card)){
-            
-        }    
-        return element;
-
+    function checkForName(element){
+        if (element.hasClass('card')){
+            return element;
+        } else {
+           return checkForName(element.parent());
+        }
     }
-    $('.card').on('click', function(){
+    $('.card').on('click', function(event){
         $('.modal-container').show();
-        
+        let nameCard = checkForName($(event.target)).attr('index');
+        let chosenCard = employeeList[nameCard];
+        modal(chosenCard);
     });
 }
     function modal(employee){
+    let options = {date: 'short' };
     let modalContainer =
     `<div class="modal-container">
             <div class="modal">
                 <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                 <div class="modal-info-container">
-                    <img class="modal-img" src="${employee.picture.medium}" alt="profile picture">
+                    <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
                     <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
                     <p class="modal-text">${employee.email}</p>
                     <p class="modal-text cap">${employee.location.city}</p>
                     <hr>
                     <p class="modal-text">${employee.cell}</p>
                     <p class="modal-text">${employee.location.street}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
-                    <p class="modal-text">${employee.dob.date}</p>
+                    <p class="modal-text">${(new Date(employee.dob.date)).toLocaleDateString("en-US", options)}</p>
                 </div>
             </div>;`
     $('body').append(modalContainer);
